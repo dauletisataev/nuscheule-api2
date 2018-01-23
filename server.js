a4bf171999364c3bc49c5e215ed12c01e95bb8b3
Daulet Issatayev <daulet.issatayev@nu.edu.kz>
2018-01-23 23:02:30 +0600

    initial commit 9

--- server.js
+++ server.js
@@ -202,6 +202,9 @@ function getFormattedTime(time, day){
     var matches = time.toLowerCase().match(/(\d{1,2}):(\d{2}) ([ap]m)/),
         output  = (parseInt(matches[1]) + (matches[3] == 'pm' ? 12 : 0)) + ':' + matches[2] + ':00',
         now = new Date();
+    if (time.startsWith("12") && time.includes("PM")){
+        output  = (parseInt(matches[1])) + ':' + matches[2] + ':00';
+    }
     var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
         year = now.getFullYear(),
         month = now.getMonth(),
